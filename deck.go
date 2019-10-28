ba6c33b2326f0c2b97e59e88fbbbd426d8153e4c
agarwalarjun123 <arjunagarwal1998@gmail.com>
2019-10-28 23:17:42 +0530

    init

---
+++ deck.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func (d deck) deal(n int) (deck, deck) {
+
+	if len(d) < n {
+		return d, deck{}
+	}
+
+	return d[:n], d[n:]
+
+}
+
+func newDeck() deck {
+
+	d := deck{}
+	cardSuit := []string{"Diamond", "Spades", "Hearts", "Club"}
+	cardValue := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, cardType := range cardSuit {
+		for _, cardNo := range cardValue {
+			d = append(d, cardNo+" of "+cardType)
+		}
+	}
+
+	return d
+}
+func (d deck) deckToString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveDeck() error {
+
+	err := ioutil.WriteFile("objectFile.txt", []byte(d.deckToString()), 0777)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func readDeck(filePath string) (deck, error) {
+	bs, err := ioutil.ReadFile(filePath)
+	if err == nil {
+		return deck(strings.Split(string(bs), ",")), err
+	}
+	return deck{}, err
+
+}
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		index := r.Intn(52)
+		d[index], d[i] = d[i], d[index]
+	}
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+
+	d := newDeck()
+	d.shuffle()
+	d.print()
+}
